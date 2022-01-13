b877b27b82b1bc8100d2836b5d1492a1e1bef4ce
ioalex <alex@alexhe.io>
2022-01-13 18:48:22 +1100

    use postcss-load-config; use dev / prod conditional

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,31 @@
-const postcssPresetEnv = require("postcss-preset-env");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
 
-module.exports = {
-  plugins: [
-    postcssPresetEnv(),
-    autoprefixer(),
-    cssnano({
-      preset: [
-        "default",
-        {
-          discardComments: {
-            removeAll: true,
-          },
-          reduceIdents: true,
-          mergeIdents: true,
-        },
-      ],
-    }),
-  ],
-};
+module.exports = (ctx) => ({
+  parser: ctx.parser ? "sugarss" : false,
+  map: ctx.env === "development" ? ctx.map : false,
+  plugins:
+    process.env.NODE_ENV === "production"
+  ? {
+      "postcss-flexbugs-fixes": {},
+      "postcss-normalize": {
+        allowDuplicates: false,
+        forceImport: "normalize/opinionated"
+      },
+      "postcss-preset-env": {},
+      autoprefixer: {},
+      cssnano: {
+            preset: [
+              "default",
+              {
+                discardComments: {
+                  removeAll: true,
+                },
+                reduceIdents: true,
+                mergeIdents: true,
+              },
+            ]
+      }
+    }
+    : {
+      // No transformations in development
+    },
+})
