7906364ea312f875a3ab4db05889c3da01ef1fd6
ioalex <alex@alexhe.io>
2022-01-16 19:03:17 +1100

    update rollup config to exclude node_modules

--- rollup.config.js
+++ rollup.config.js
@@ -15,9 +15,10 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
-    production && babel({ 
-      babelHelpers: "bundled",
-      exclude: "node_modules/**"
+    production &&
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
       }),
     production &&
       terser({
